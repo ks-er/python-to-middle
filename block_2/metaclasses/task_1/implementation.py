5cce7387fa9669b6e16c8dab6ed089b078decb85
ks_e <knershova@gmail.com>
2024-07-14 13:12:10 +0300

    Update implementation.py

--- block_2/metaclasses/task_1/implementation.py
+++ block_2/metaclasses/task_1/implementation.py
@@ -1,2 +1,14 @@
 class Updateable(type):
-    pass
+
+    __newCls = None
+
+    def __new__(cls, name, bases, kwargs):
+        if not cls.__newCls:
+            cls.__newCls = type.__new__(cls, name, bases, kwargs)
+        return cls.__newCls
+
+    def __init__(self, name, bases, kwargs):
+        attrs = ((name, value) for name, value in kwargs.items() if not name.startswith('__'))
+
+        for name, value in attrs:
+            setattr(self, name, value)
