0eaf7d9ace6b5599195cf6e5e2b279d1b9ad908e
ks_e <knershova@gmail.com>
2024-07-21 13:28:35 +0300

    Update implementation.py

--- block_2/metaclasses/task_2/implementation.py
+++ block_2/metaclasses/task_2/implementation.py
@@ -1,4 +1,41 @@
+class LazyMeta(type):
+
+    def __call__(cls, expression, *args):
+        expression_result = expression()
+        new_cls = LazyMeta.__new__(
+            LazyMeta,
+            cls.__name__,
+            (type(expression_result),),
+            { expression.__name__: expression }
+        )
+
+        instance = super().__call__(new_cls, expression, expression_result, *args)
+
+        return instance
+
+    def __new__(self, cls_name, expression_result, dict):
+        new_cls = super().__new__(
+            self,
+            cls_name,
+            expression_result,
+            dict)
+
+        return new_cls
+
+
 class Lazy(metaclass=LazyMeta):
-    def __init__(self, expression) -> None:
-        super().__init__()
-        object.__setattr__(self, 'expression', expression)
+
+    def __new__(self, cls_instance, expression, expression_result, *args):
+        # Проверка на class
+        if hasattr(expression_result, '__module__'):
+            res = cls_instance.__new__(
+                cls_instance
+            )
+        else:
+            res = cls_instance.__new__(
+                cls_instance,
+                expression_result,
+                *args
+            )
+
+        return res
